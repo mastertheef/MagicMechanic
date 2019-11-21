98ba475fab455f46b57200bfcfbeabe880ecbf49
Mykola Shvydkyy <mykola_shvydkyy@epam.com>
2019-11-21 12:43:59 +0200

    fix attacking after move

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,9 +3,6 @@ using UnityEngine.InputSystem;
 
 public class InputManager : SingletonBase<InputManager>
 {
-    private Ray cameraToGroundRay;
-    private RaycastHit groundHit;
-
     public void OnAttack(InputAction.CallbackContext ctx)
     {
         switch (ctx.phase)
@@ -14,7 +11,6 @@ public class InputManager : SingletonBase<InputManager>
                 PlayerStateManager.Instance.SetState(PlayerState.Attacking);
                 break;
             case InputActionPhase.Canceled:
-            default:
                 PlayerStateManager.Instance.SetState(PlayerState.Idle);
                 break;
         }
